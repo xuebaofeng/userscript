c81dfbd917beef2a40cf05ca702f863fa56a4ef2
xuebaofeng <xbaofeng@gmail.com>
2014-02-11 16:09:27 +0800

    Update OracleMonkey.user.js

--- src/OracleMonkey.user.js
+++ src/OracleMonkey.user.js
@@ -6,7 +6,7 @@
 // @include     http://*.us.oracle.com*
 // @include     http://*.oraclecorp.com/*
 // @include     https://*.oraclecorp.com/*
-// @version     1.8.4
+// @version     1.8.5
 // @grant       GM_log
 // @grant       GM_setValue
 // @grant       GM_getValue
