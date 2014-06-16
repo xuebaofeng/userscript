295f5430a17235cb9ddc6340946fd89fa0ad2608
xuebaofeng <xbaofeng@gmail.com>
2014-06-16 09:30:27 +0800

    Update ChinazMonkey.user.js

--- src/ChinazMonkey.user.js
+++ src/ChinazMonkey.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name       ChinazMonkey
 // @namespace  chinaz.com
-// @version    1.2
+// @version    1.1
 // @description  站长之家文章阅读简化
 // @match      http://www.chinaz.com/*.shtml
+// @match      http://*.sina.com.cn/*.shtml
 // @copyright  GNU
 // @require     http://code.jquery.com/jquery-2.1.1.min.js
 // @run-at      document-end
@@ -11,12 +12,30 @@
 // @downloadURL https://github.com/xuebaofeng/userscript/raw/master/src/ChinazMonkey.user.js
 // ==/UserScript==
 
+var url = window.location.href;
+if(url.indexOf('chinaz')>=0){
 console.log('chinaz begin');
 
 $('#cz-head,.m-crumb-search,.cz-box-300,#cz-footer,.u-postfooter,.m-relate,.m-leftad,.m-picshow,#pinglun').remove();
-
 $('#content').removeClass('cz-box-670');
 $('.m-post').removeClass('m-post').css('background','#F5FAFF');
 $('#fulltext').click();
 
 console.log('chinaz end');
+return;
+}
+
+
+if(url.indexOf('sina')>=0){
+console.log('sina begin');
+
+$('#navTop,#hdnav,#blkBreadcrumb,.sidebar,.can_right,.wb_rec,.wc14_qr,.guess-view-list,.blkContainerOther,#J_Comment_Form_B').remove();
+
+$('.blkContainerSblk').removeClass('blkContainerSblk');
+$('.blkContainer').removeClass('blkContainer');
+     
+console.log('sina end');
+return;
+}
+
+
