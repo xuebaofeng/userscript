ca36a4e379f04e70a47890798b35c826eca7b089
xuebaofeng <xbaofeng@gmail.com>
2014-08-07 13:46:06 +0800

    c

--- src/ReadingMonkey.user.js
+++ src/ReadingMonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       ReadingMonkey
 // @namespace  baofeng.reading
-// @version    3.3
+// @version    3.4
 // @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易
 // @match      http://www.chinaz.com/*.shtml
 // @match      http://*.sina.com.cn/*.shtml
@@ -57,8 +57,8 @@
         '#wrapper,#site,#content,.article_page_left');
 
     simplify('oschina.net',
-        '#OSC_NavTop,#OSC_Banner,#OSC_Footer,.NewsRight,.toolbar,.copyright,.RelatedNews,.RelatedThreads,#upprev_box',
-        '#OSC_Screen,.NewsBody,.NewsEntity');
+        '#OSC_NavTop,#OSC_Banner,#OSC_Footer,.NewsRight,.toolbar,.copyright,.RelatedNews,.RelatedThreads,#upprev_box,.translater',
+        '#OSC_Screen,.NewsBody,.NewsEntity,.TextContent');
 
     simplify('163.com',
         '#js_N_nav,.ep-header,.subfoot-wrap,.N-nav-bottom,#epContentRight,.sharecommend-wrap,.ep-keywords,.atleLP,.ep-returnlink,.extra-tag',
