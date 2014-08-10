c1aaaf0fcecfedd9a0eaa0d7427eafd35558a041
Baofeng Xue <xbaofeng@gmail.com>
2014-08-10 20:54:19 +0800

    c

--- src/ReadingMonkey.user.js
+++ src/ReadingMonkey.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name       ReadingMonkey
 // @namespace  baofeng.reading
-// @version    3.9
-// @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易
+// @version    4.0
+// @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易,伯乐在线
 // @match      http://www.chinaz.com/*.shtml
 // @match      http://*.sina.com.cn/*htm*
 // @match      http://www.admin10000.com/document/*.html
@@ -14,6 +14,7 @@
 // @match      http://www.infoq.com/*
 // @match      http://www.oschina.net/*
 // @match      http://*.163.com/*.html*
+// @match      http://*.jobbole.com/*
 // @copyright  GNU
 // @require     http://code.jquery.com/jquery-2.1.1.min.js
 // @run-at      document-end
@@ -28,11 +29,11 @@
         '.navWrap,.foot-Article-QQ,#sideBars,.crumbs-tool,#about,.ft,#scrollBtn', '.body-Article-QQ,#Main-Article-QQ,.main');
 
     simplify('cnblogs.com',
-        '#header,#footer,#sideBar,#blog_news_kb,#wrap,#main_header,#sideright,#guide,.blogStats,#leftcontent,#mylinks,#right,#mytopmenu,.footer',
+        '#sideBar,#blog_news_kb,#wrap,#main_header,#sideright,#guide,.blogStats,#leftcontent,#mylinks,#right,#mytopmenu',
         '#main_wrapper,#sideleft,#main,#home,#mainContent,.post,#centercontent,#left');
 
     simplify('blog.csdn.net',
-        '.csdn-toolbar,#header,#navigator,#side,.notice,#res-relatived,.blog-associat-tag,.tag_list,#pub_footerall',
+        '.csdn-toolbar,#navigator,#side,.notice,#res-relatived,.blog-associat-tag,.tag_list,#pub_footerall',
         '#content,#body,#main,.main,.details,.post');
 
     simplify('chinaz.com',
@@ -40,19 +41,19 @@
         '.czbox,#content,.m-post');
 
     simplify('sina.com',
-        '#navTop,#hdnav,#blkBreadcrumb,.sidebar,.can_right,.wb_rec,.wc14_qr,.guess-view-list,.blkContainerOther,#J_Comment_Form_B,.side-btns-2wm,\
-.navTop,.blkBreadcrumb,.footer,.nsinatopbar,#sinablogHead,#column_1,#sinablogfooter,.SG_connHead,#blk_nav_1,.topbar',
+        '#navTop,#hdnav,#blkBreadcrumb,.can_right,.wb_rec,.wc14_qr,.guess-view-list,.blkContainerOther,#J_Comment_Form_B,.side-btns-2wm,\
+.navTop,.blkBreadcrumb,,.nsinatopbar,#sinablogHead,#column_1,#sinablogfooter,.SG_connHead,#blk_nav_1,.topbar',
         '.wrap,.blkContainerSblk,.blkContainer,#column_2,#sinablogbody,.articalContent,.SG_connBody,.blkContainerSblk,.Main');
 
     simplify('admin10000.com',
-        '#miniNav,#header,#nav,#search,#position,#footer,.right,.weixin,.tags,.tip,.relation,.share,.texttip,.ad_336x280,.ad_640x90',
+        '#miniNav,#search,#position,.right,.weixin,.tags,.tip,.relation,.share,.texttip,.ad_336x280,.ad_640x90',
         '.left,#main');
 
     simplify('51cto.com',
         '#home_top,.headerBox,.blogLeft,.mainNav,.edu-col-b,.relatedArt,#message,.mb10,.menu,.subweb,.g_13,.tips,.titbg,.reltag,.relart,.cathot,.bor,#ft,.crumb',
         '.blogMain,.blogRight,.g_26,.g_39,.brief ');
 
-    simplify('infoq.com', '#topInfo,#header,#footer,.share_this,.article_page_right,.random_links,.bottomContent',
+    simplify('infoq.com', '#topInfo,.share_this,.article_page_right,.random_links,.bottomContent',
         '#wrapper,#site,#content,.article_page_left');
 
     simplify('oschina.net',
@@ -63,6 +64,9 @@
         '#js_N_nav,.ep-header,.subfoot-wrap,.N-nav-bottom,#epContentRight,.sharecommend-wrap,.ep-keywords,.atleLP,.ep-returnlink,.extra-tag',
         '#js-epContent,#epContentLeft,#endText');
 
+    simplify('jobbole.com',
+        '#wp_rp_first',
+        '.container,.grid-8');
 
     function simplify(siteName, removeStr, expandStr, callback) {
 
@@ -79,9 +83,11 @@
             .css('text-indent', '1em');
 
 
-        setInterval(function () {
+        $('header,nav,footer,iframe,#header,#footer,#sidebar,.sidebar,#breadcrumb,.header,.footer,#nav').remove();
+
+        setTimeout(function () {
             $('iframe').remove();
-        }, 1000);
+        }, 3000);
 
 
         if (url.indexOf(siteName) >= 0) {
