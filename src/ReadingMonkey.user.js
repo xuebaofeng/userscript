de4df8f7e013ad5c96520d982da41525ad9f7c4f
xuebaofeng <xbaofeng@gmail.com>
2014-08-15 13:43:07 +0800

    c

--- src/ReadingMonkey.user.js
+++ src/ReadingMonkey.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name       ReadingMonkey
 // @namespace  baofeng.reading
-// @version    4.6
-// @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易,伯乐在线,feedly,炼数成金
+// @version    4.8
+// @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易,伯乐在线,feedly,炼数成金 dataguru.cn
 // @match      http://www.chinaz.com/*.shtml
 // @match      http://*.sina.com.cn/*htm*
 // @match      http://www.admin10000.com/document/*.html
 // @match      http://*.51cto.com/*
-// @match      http://blog.csdn.net/*/article/details/*
+// @match      http://*.csdn.net/*
 // @match      http://www.cnblogs.com/*/*.html
 // @match      http://news.cnblogs.com/n/*
 // @match      http://*.qq.com/*.htm*
@@ -16,7 +16,7 @@
 // @match      http://*.163.com/*.html*
 // @match      http://*.jobbole.com/*
 // @match      http://feedly.com/*
-// @match      http://www.dataguru.cn/portal.php?mod=view&aid=*
+// @match      http://www.dataguru.cn/*
 // @copyright  GNU
 // @require     http://code.jquery.com/jquery-2.1.1.min.js
 // @run-at      document-end
@@ -27,36 +27,38 @@
 (function () {
     var url = window.location.href;
 
+
     simplify('qq.com',
-        '.navWrap,.foot-Article-QQ,#sideBars,.crumbs-tool,#about,.ft,#scrollBtn', '.body-Article-QQ,#Main-Article-QQ,.main');
+        '.navWrap,.foot-Article-QQ,.crumbs-tool,#about,.ft,#scrollBtn,#invideocon,#sideBars',
+        '.body-Article-QQ,#Main-Article-QQ');
 
     simplify('cnblogs.com',
-        '#sideBar,#blog_news_kb,#wrap,#main_header,#sideright,#guide,.blogStats,#leftcontent,#mylinks,#right,#mytopmenu',
-        '#main_wrapper,#sideleft,#main,#home,#mainContent,.post,#centercontent,#left');
+        '#blog_news_kb,#wrap,#main_header,#sideright,#guide,.blogStats,#leftcontent,#mylinks,#right,#mytopmenu',
+        '#main_wrapper,#sideleft,#mainContent,#centercontent,.forFlow');
 
-    simplify('blog.csdn.net',
+    simplify('csdn.net',
         '.csdn-toolbar,#navigator,#side,.notice,#res-relatived,.blog-associat-tag,.tag_list,#pub_footerall',
-        '#content,#body,#main,.main,.details,.post');
+        '.details');
 
     simplify('chinaz.com',
         '#cz-head,.m-crumb-search,.cz-box-300,#cz-footer,.u-postfooter,.m-relate,.m-leftad,.m-picshow,#pinglun,#m-rightshare,.u-post-textad',
-        '.czbox,#content,.m-post');
+        '.czbox,.m-post');
 
     simplify('sina.com',
-        '#navTop,#hdnav,#blkBreadcrumb,.can_right,.wb_rec,.wc14_qr,.guess-view-list,.blkContainerOther,#J_Comment_Form_B,.side-btns-2wm,\
-.navTop,.blkBreadcrumb,.nsinatopbar,#sinablogHead,#column_1,#sinablogfooter,.SG_connHead,#blk_nav_1,.topbar,.zwsidebar,.headlines_news,#fudong',
-        '.wrap,.blkContainerSblk,.blkContainer,#column_2,#sinablogbody,.articalContent,.SG_connBody,.blkContainerSblk,.Main,.zwcontent,.zwc');
+        '#hdnav,#blkBreadcrumb,.can_right,.wb_rec,.wc14_qr,.guess-view-list,.blkContainerOther,#J_Comment_Form_B,.side-btns-2wm,\
+.blkBreadcrumb,.nsinatopbar,#sinablogHead,#column_1,#sinablogfooter,.SG_connHead,#blk_nav_1,.topbar,.zwsidebar,.headlines_news,#fudong',
+        '.blkContainerSblk,.blkContainer,#column_2,#sinablogbody,.articalContent,.SG_connBody,.blkContainerSblk,.Main,.zwcontent,.zwc');
 
     simplify('admin10000.com',
         '#miniNav,#search,#position,.right,.weixin,.tags,.tip,.relation,.share,.texttip,.ad_336x280,.ad_640x90',
-        '.left,#main');
+        '');
 
     simplify('51cto.com',
         '#home_top,.headerBox,.blogLeft,.mainNav,.edu-col-b,.relatedArt,#message,.mb10,.menu,.subweb,.g_13,.tips,.titbg,.reltag,.relart,.cathot,.bor,#ft,.crumb',
         '.blogMain,.blogRight,.g_26,.g_39,.brief ');
 
     simplify('infoq.com', '#topInfo,.share_this,.article_page_right,.random_links,.bottomContent,.eBookLeft',
-        '#wrapper,#site,#content,.article_page_left,.ebook,.txt');
+        '#site,.article_page_left,.ebook,.txt');
 
     simplify('oschina.net',
         '#OSC_NavTop,#OSC_Banner,#OSC_Footer,.NewsRight,.toolbar,.copyright,.RelatedNews,.RelatedThreads,#upprev_box,.translater,.ProjectRight',
@@ -68,13 +70,13 @@
 
 
     simplify('jobbole.com',
-        '#wp_rp_first',
-        '.container,.grid-8');
+        '#wp_rp_first,.right-content',
+        '.grid-8,.col-sm-8');
 
 
     simplify('dataguru.cn',
         '#float_l,#float_r,#friendlink,.sd,#pt,.wrap_top,.wrap_end',
-        '.wrap,.mn,#ct:first-child,.wrap_main,#wp', function () {
+        '.mn,#ct:first-child,.wrap_main,#wp', function () {
             $('#wp').parent().css('width', '100%');
         });
 
@@ -86,48 +88,70 @@
     });
 
 
-    function simplify(siteName, removeStr, expandStr, callback) {
+    function expand(expandStr) {
+        if (expandStr && expandStr !== '') {
+            expandStr += ',.';
+        }
+
+        var expandArray = ['main', 'home', 'body', 'container', 'content', 'wrapper', 'left', 'right', 'post', 'wrap'];
+
+        expandStr += expandArray.join(',.');
+        expandStr += ',#' + expandArray.join(',#');
+
+        $(expandStr).css({
+                'width': '100%',
+                'padding': 0,
+                'border': 0,
+                'margin': 0,
+                'background-image': 'none',
+                'background-color': '#F5FAFF',
+                'max-width': 'none'
+            }
+        );
+        console.log('elements expanded');
+
+    }
+
+    function remove(removeStr) {
+        if (removeStr && removeStr !== '') {
+            removeStr += ',';
+        }
+
+        var removeArray = ['header', 'nav', 'footer', 'embed', 'iframe', 'sidebar', 'breadcrumb', 'sideBar', 'navTop'];
+
+        removeStr += removeArray.join(',');
+        removeStr += ',#' + removeArray.join(',#');
+        removeStr += ',.' + removeArray.join(',.');
+        $(removeStr).remove();
+        console.log('elements removed');
+    }
 
 
-        if (url.indexOf(siteName) == -1) return;
-        console.log(siteName + ' begin');
-
+    function addStyle() {
         $("<style type='text/css'>\
 body{margin:0;border:0;padding-left:20px;padding-right:20px;background-image:none;width:auto;}\
 p{font-family:Georgia, \"Times New Roman\", \"Microsoft YaHei\", \"微软雅黑\", STXihei, \"华文细黑\", serif;font-size:large;color:#000000;text-indent:1em;}\
 </style>").appendTo("head");
-
         console.log('style append');
+    }
 
+    function simplify(siteName, removeStr, expandStr, callback) {
 
-        $('header,nav,footer,iframe,#header,#footer,#sidebar,.sidebar,#breadcrumb,.header,.footer,#nav,iframe,embed,.nav').remove();
-        console.log('common removed');
+        if (url.indexOf(siteName) == -1) return;
+        console.log(siteName + ' begin');
 
-        setTimeout(function () {
-            $('iframe,embed').remove();
-        }, 3000);
+        addStyle();
 
-        if (removeStr) {
-            $(removeStr).remove();
-            console.log('elements removed');
-        }
-        if (expandStr) {
-            $(expandStr).css({
-                    'width': '100%',
-                    'padding': 0,
-                    'border': 0,
-                    'margin': 0,
-                    'background-image': 'none',
-                    'background-color': '#F5FAFF',
-                    'max-width': 'none'
-                }
-            );
-            console.log('elements expanded');
-        }
+        remove(removeStr);
+        expand(expandStr);
 
         if (callback) {
             callback();
         }
+
+        setTimeout(function () {
+            $('iframe,embed').remove();
+        }, 3000);
 
         console.log(siteName + ' end');
 
