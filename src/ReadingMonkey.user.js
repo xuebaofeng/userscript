10228ad4364a4c666437fd7c6bc427d895b435df
Baofeng Xue <xbaofeng@gmail.com>
2015-04-17 18:31:21 +0800

    Update ReadingMonkey.user.js

--- src/ReadingMonkey.user.js
+++ src/ReadingMonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       ReadingMonkey
 // @namespace  baofeng.reading
-// @version    10s
+// @version    10
 // @description  文章阅读简化:站长之家,新浪,web开发者,51cto,csdn,博客园,qq,infoq,开源中国,网易,伯乐在线,feedly,炼数成金, dataguru.cn, linuxeden,managershare
 // @match      http://www.chinaz.com/*.shtml
 // @match      http://sports.sina.com.cn/*htm*
