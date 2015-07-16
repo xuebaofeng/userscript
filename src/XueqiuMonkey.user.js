040bde303f8c907a40f40443a91d5a5afcc10584
Baofeng Xue <xbaofeng@gmail.com>
2015-07-16 19:20:12 +0800

    Update XueqiuMonkey.user.js

--- src/XueqiuMonkey.user.js
+++ src/XueqiuMonkey.user.js
@@ -3,7 +3,7 @@
 // @namespace   bf
 // @description 雪球股票详情页，一键打开相关页面： i财富，牛叉诊股，千股千评，股吧
 // @include     http://xueqiu.com/S/*
-// @version     2
+// @version     3
 // @grant       none
 // @copyright  GNU
 // @run-at      document-end
@@ -14,18 +14,6 @@
 (function () {
 
     console.log('xueqiu begin')
-
-    function addLink(ele, name, href) {
-
-        var a = document.createElement("a")
-        var t = document.createTextNode(name)
-        a.appendChild(t)
-        a.href = href
-        console.log(a)
-        ele.appendChild(a)
-        ele.appendChild(document.createElement("br"))
-    }
-
 
     function buildStock() {
         var url = document.location.href
@@ -41,12 +29,17 @@
     var t = document.createTextNode('油猴数据')
     a.appendChild(t)
     a.onclick = function () {
-        window.open('http://www.icaifu.com/stock/doctora/' + stock + '.shtml')
-        window.open('http://qgqp.shdjt.com/gpdm.asp?gpdm=' + stock.substring(2))
-        window.open('http://guba.eastmoney.com/list,' + stock.substring(2) + '.html')
+        var urls=[
+            'http://www.icaifu.com/stock/doctora/' + stock + '.shtml',
+                  'http://qgqp.shdjt.com/gpdm.asp?gpdm=' + stock.substring(2),
+                  'http://guba.eastmoney.com/list,' + stock.substring(2) + '.html',
+                  'http://doctor.10jqka.com.cn/' + stock.substring(2) + '/'
+                 ]
+        for(var i=0;i<urls.length;i++){
+            window.open(urls[i],urls[i])
+        }
     }
     ele.appendChild(a)
-
 
     console.log('xueqiu end')
 })()
