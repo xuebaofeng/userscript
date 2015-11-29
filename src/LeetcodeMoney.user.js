2bef0c5186e77fda7868f4df47d7e8c96e78b9ff
xuebaofeng <xbaofeng@gmail.com>
2015-11-29 12:23:29 -0800

    add new

---
+++ src/LeetcodeMoney.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        LeetcodeMoney
+// @namespace   bf
+// @description remove locked item from list
+// @include     https://leetcode.com/problemset/algorithms/
+// @version     1
+// @grant       none
+// @downloadURL https://github.com/xuebaofeng/userscript/raw/master/src/TvMonkey.user.js
+// ==/UserScript==
+
+
+(function () {
+
+    console.log('LeetcodeMoney begin')
+
+    document.addEventListener('DOMContentLoaded', function (event) {
+        hideLocked();
+    });
+
+
+    function hideLocked() {
+        var x = document.querySelectorAll('#problemList i.fa.fa-lock')
+        for (i = 0; i < x.length; i++) {
+            x[i].parentNode.parentNode.classList.add('hide');
+        }
+    }
+
+    console.log('LeetcodeMoney end')
+})();
