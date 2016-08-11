a53549886e3acff27ed6ced8ac057fd8d806dd2a
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2016-08-11 11:44:28 -0700

    Update SapMonkey.user.js

--- src/SapMonkey.user.js
+++ src/SapMonkey.user.js
@@ -2,12 +2,31 @@
 // @name        SapMonkey
 // @namespace   bf.sap
 // @include     https://confluence.successfactors.com/*
+// @include     http://192.168.163.128:8080/*
 // @downloadURL https://github.com/xuebaofeng/userscript/raw/master/src/SapMonkey.user.js
-// @version     1
+// @run-at      document-end
+// @version     2
 // ==/UserScript==
 (function () {
-    var url = window.location.href
-    if (url.indexOf('https://confluence.successfactors.com') == 0) {
-        document.querySelector('#header-precursor').style.display = 'none'
-    }
-})()
+  var url = window.location.href
+  if (url.indexOf('https://confluence.successfactors.com') == 0) {
+    document.querySelector('#header-precursor').style.display = 'none'
+  } //
+
+  if (url.indexOf('http://192.168.163.128:8080') == 0) {
+    setInterval(function () {
+      var c = document.querySelector('input[placeholder="Enter Company ID"]')
+      console.log(c)
+      if (c) {
+        c.value = 'salesdemo'
+        document.href = 'http://192.168.163.128:8080/login?company=salesdemo#/login'
+      }
+      c = document.querySelector('input[placeholder="Username"]')
+      console.log(c)
+      if (c) {
+        c.value = 'bx'
+        document.querySelector('input[placeholder="Enter Password"]').value = 'pwd1'
+      }
+    }, 1000);
+  }
+}) ()
