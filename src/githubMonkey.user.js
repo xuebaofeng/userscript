07346f92f3c11a730d1b719819f0b8383dee84e5
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2017-08-11 10:45:59 -0700

    Create githubMonkey.user.js

---
+++ src/githubMonkey.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Reload GitHub Pull Request Diff page to ignore whitespace
+// @namespace   bf
+// @description Reloads the GitHub Pull Request Diff page, adjusting the URL to have the diffs ignore whitespace.
+// @include     https://github.com/*/pull/*/files
+// @include     https://github.wdf.sap.corp/*/commit/*
+// @exclude     https://*?w=1
+// @grant       none
+// @version     1.1
+// ==/UserScript==
+
+// Wipe out the head and body contents so that we don't wait for them to load before doing the redirect.
+document.head.innerHTML='';
+document.body.innerHTML='';
+
+// Redirect to the URL we are at, with the special modifier attached that signals to ignore whitespace in the diffs.
+window.location.href=window.location.href + '?w=1';
