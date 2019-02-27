ecadf927ed9d10f0259aed388429c3a760a8539d
Baofeng(Shawn) Xue <xbaofeng@gmail.com>
2019-02-26 16:12:42 -0800

    Update KantieMonkey.user.js

--- src/KantieMonkey.user.js
+++ src/KantieMonkey.user.js
@@ -2,7 +2,7 @@
 // @name        KantieMonkey
 // @namespace   bf.Kantie
 // @downloadURL https://github.com/xuebaofeng/userscript/raw/master/src/KantieMonkey.user.js
-// @version     1
+// @version     2
 // @match        https://kantie.org/topics/today
 // ==/UserScript==
 
@@ -11,8 +11,10 @@
     'use strict';
 
     removeHupu()
+    removeDuplicate()
     document.body.addEventListener('DOMSubtreeModified', function() {
         removeHupu()
+        removeDuplicate()
     }, false);
 
 })();
@@ -22,4 +24,15 @@ function removeHupu() {
     for (var i = 0; i < list.length; i++) {
         list[i].parentNode.parentNode.remove();
     }
+}
+
+function removeDuplicate() {
+    var titles = new Set();
+    var list = document.querySelectorAll('a.content > div.title')
+    for (var i = 0; i < list.length; i++) {
+        var title = list[i].innerText
+        if(titles.has(title)) list[i].parentNode.parentNode.parentNode.remove();
+        titles.add(title);
+    }
+    console.log(titles);
 }
